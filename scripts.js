9b25dc4b47f6fc05b933fbb58e70ae04f682d56f
Ayesha Khan <ayeshaa.k3002@gmail.com>
2024-05-31 10:42:51 -0400

    Rename file

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// Google Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-89YPW531YB');
+
+// AOS Initialization
+AOS.init({
+    duration: 1200,})
+    window.addEventListener('load', AOS.refresh)
